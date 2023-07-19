2224e9befe952ece989e1b77fef8380495f9800d
Nikita Komissarov <r.c-dm@yandex.ru>
2023-07-19 19:52:32 +0300

    Boolean without 'n','y'

--- src/Rules/Boolean.php
+++ src/Rules/Boolean.php
@@ -18,6 +18,6 @@ class Boolean extends Rule
      */
     public function check($value): bool
     {
-        return \in_array($value, [\true, \false, "true", "false", 1, 0, "0", "1", "y", "n"], \true);
+        return \in_array($value, [\true, \false, "true", "false", 1, 0, "0", "1"], \true);
     }
 }
